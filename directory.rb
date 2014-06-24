3485059804d41850e5b3baf9ab64227fb1e3663c
Lisa A-R <lisaar@gmail.com>
2014-06-24 12:03:08 +0100

    Array created and printed

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
+# This code defines the students in the cohort inside of an array
+students = [
+	"Chloe", 
+	"Hannah Waxman", 
+	"Zoe Bryant", 
+	"Lisa Altmann-Richer", 
+	"Marco Araujo", 
+	"Jenny Wang", 
+	"Nicola Aitken", 
+	"Jean", 
+	"Eddie"
+]
 #This code prints the name of the students of June cohort 2014 of makers academy
 puts "The students of my cohort at Makers Academy"
 puts "-----------------"
-puts "Chloe"
-puts "Hannah Waxman"
-puts "Zoe Bryant"
-puts "Lisa Altmann-Richer"
-puts "Marco Araujo"
-puts "Jenny Wnag"
-puts "NIcola Aitken"
-puts "Jean"
-puts "Eddie"
-
-#Variable to create student count
-student_count = 9
+students.each do |student|
+	puts student
+end
 
 #This code prints the number of students in the cohort
-print "Overall, we have #{student_count} students in our cohort"
+puts "Overall, we have #{students.length} students in our cohort"
